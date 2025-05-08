9f751ab701dc09b0698592ded77ecd377ee17259
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2025-05-08 12:57:34 +0200

    Implementar Dokka y hacer la documentacion web

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,26 @@
+
 plugins {
     kotlin("jvm") version "2.0.21"
+    application
+    id("org.jetbrains.dokka") version "1.9.0"
 }
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
     jvmToolchain(21)
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+tasks.dokkaHtml {
+    outputDirectory.set(file("build/dokka"))
 }
